74006b830d30a65a5d461965f7ee1c5cb8121e23
Miriam López Sierra <mirlop01@ucm.es>
2021-01-18 21:39:43 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,10 +5,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 let urlDB = "";
 if (process.env.NODE_ENV === "dev") {
 	urlDB = "mongodb://localhost:27017/sokery_db";
-} else {
-	urlDB =
-		"mongodb+srv://mirlop01:Mufasa12.@mirlop01tfg.ku8e6.mongodb.net/mirlop01?retryWrites=true&w=majority";
-}
+} 
 
 process.env.URLDB = urlDB;
 process.env.CADUCIDAD_TOKEN = "48h";
