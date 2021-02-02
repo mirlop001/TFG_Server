d6d6bf96de66242f2b83cd9b394581e8880d163d
Miriam López Sierra <mirlop01@ucm.es>
2021-02-02 22:10:58 +0100

    connection bug fixing

--- config/prod-database.js
+++ config/prod-database.js
@@ -1,13 +1,23 @@
+process.env.PORT = process.env.PORT || 8080;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 let urlDB = "";
+let seed = "";
+let token = "";
+
 if (process.env.NODE_ENV === "dev") {
 	urlDB = "mongodb://localhost:27017/sokery_db";
+	seed = "este-es-el-seed-desarrollo";
+	token = "48h";
 } else {
 	urlDB = `${APPSETTING_URLDB}`;
+	seed = `${APPSETTING_SEED_AUTH}`;
+	token = `${APP_SETTING_CADUCIDAD_TOKEN}`;
 }
 
 process.env.URLDB = urlDB;
+process.env.CADUCIDAD_TOKEN = token;
+process.env.SEED_AUTENTICACION = seed;
 
 module.exports = {
 	url: process.env.URLDB,
